2aff7f14a7032d8ab68c65740afcdf105274d9eb
Joao De Oliveira Pinheiro <deolj19@wfu.edu>
2023-08-29 22:33:23 -0400

    Final Function Commit

--- Apply_Approx_Hessian.m
+++ Apply_Approx_Hessian.m
@@ -1,18 +1,46 @@
-function JJW = Apply_Approx_Hessian(Mat, M, lambda)
+function JJx = Apply_Approx_Hessian(CI_Mat, x, varargin)
+%   Apply_Approx_Hessian applies the approximate hessian J'J implicitly.
+%
+%   JJx = Apply_Approx_Hessian(CI_Mat, x) implicitly applies the 
+%   approximate hessian using the cyclic invariant matrices in CI_Mat to
+%   vector x.
+%
+%   JJx = Apply_Approx_Hessian(CI_Mat, x, param=value) specifies additional
+%   parameters for the method. Specifically...
+%
+%   'beta' - correction parameter {0}
+%   'lambda' - Damping factor {0}
+
+    %% Set up parameters
+    params = inputParser;
+    params.addParameter('beta', 0, @(x) isscalar(x) & x >= 0)
+    params.addParameter('lambda', 0, @(x) isscalar(x) & x >= 0);
+    params.parse(varargin{:});
+
+    %% Copy from params object
+    beta = params.Results.beta;
+    lambda = params.Results.lambda;
+   
+    %% Get Values
     % Get matrices in cell
-    S = Mat{1};
-    U = Mat{2};
-    V = Mat{3};
-    W = Mat{4};
+    S = CI_Mat{1};
+    U = CI_Mat{2};
+    V = CI_Mat{3};
+    W = CI_Mat{4};
+    
+    % Get Sizes of tensor and chosen decompositions
+    n = size(S, 1);
+    Rs = size(S, 2);
+    Rc = size(U, 2);
     
     % Get subvectors
-    A = ci_tt_cp_vec_to_fac(M,Mat);
+    A = ci_tt_cp_vec_to_fac(x,CI_Mat);
     Ms = A{1};
     Mu = A{2};
     Mv = A{3};
     Mw = A{4};
-
-    % Transpose Precomputations
+    
+    %% Transpose Precomputations
     % S operations
     SS = S'*S;
     SU = S'*U;
@@ -33,49 +61,48 @@ function JJW = Apply_Approx_Hessian(Mat, M, lambda)
     WU = W'*U;
     WV = W'*V;
     WW = W'*W;
-    % M operations
-    % Z operations
+    % Ms operations
     MsS = Ms'*S;
     MsU = Ms'*U;
     MsV = Ms'*V;
     MsW = Ms'*W;
-    % X operations
+    % Mu operations
     MuS = Mu'*S;
     MuU = Mu'*U;
     MuV = Mu'*V;
     MuW = Mu'*W;
-    % Y operations
+    % Mv operations
     MvS = Mv'*S;
     MvU = Mv'*U;
     MvV = Mv'*V;
     MvW = Mv'*W;
-    % K operations
+    % Mw operations
     MwS = Mw'*S;
     MwU = Mw'*U;
     MwV = Mw'*V;
     MwW = Mw'*W;
         
-    % Apply Approx Hessian to Vector 
+    %% Apply Approx Hessian to Vector 
     % Js Operations
-    OneA = 3*(Ms*(SS.*SS) + 2*(S*(MsS.*SS))); % Correct
-    OneB = 3*(Mu*(VS.*WS) + V*(MuS.*WS) + W*(MuS.*VS));
-    OneC = 3*(Mv*(US.*WS) + U*(MvS.*WS) + W*(MvS.*US));
-    OneD = 3*(Mw*(US.*VS) + U*(MwS.*VS) + V*(MwS.*US));
+    OneA = 3*(Ms*(SS.*SS) + 2*(S*(MsS.*SS))) + beta*Ms; %Js'*Js*vec(Ms)
+    OneB = 3*(Mu*(VS.*WS) + V*(MuS.*WS) + W*(MuS.*VS)); %Js'*Ju*vec(Mu)
+    OneC = 3*(Mv*(US.*WS) + U*(MvS.*WS) + W*(MvS.*US)); %Js'*Jv*vec(Mv)
+    OneD = 3*(Mw*(US.*VS) + U*(MwS.*VS) + V*(MwS.*US)); %Jw'*Jw*vec(Mw)
     % Ju Operations
-    TwoA = 3*(Ms*(SV.*SW) + S*((MsW.*SV) + (MsV.*SW)));
-    TwoB = 3*(Mu*(VV.*WW) + V*(MuW.*WV) + W*(MuV.*VW));
-    TwoC = 3*(Mv*(WV.*UW) + W*(MvW.*UV) + U*(MvV.*WW));
-    TwoD = 3*(Mw*(UV.*VW) + U*(MwW.*VV) + V*(MwV.*UW));
+    TwoA = 3*(Ms*(SV.*SW) + S*((MsW.*SV) + (MsV.*SW))); %Ju'*Js*vec(Ms)
+    TwoB = 3*(Mu*(VV.*WW) + V*(MuW.*WV) + W*(MuV.*VW)) + beta*Mu; %Ju'*Ju*vec(Mu)
+    TwoC = 3*(Mv*(WV.*UW) + W*(MvW.*UV) + U*(MvV.*WW)); %Ju'*Jv*vec(Mv)
+    TwoD = 3*(Mw*(UV.*VW) + U*(MwW.*VV) + V*(MwV.*UW)); %Ju'*Jw*vec(Mw)
     % Jv Operations
-    ThreeA = 3*(Ms*(SU.*SW) + S*((MsU.*SW) + (MsW.*SU)));
-    ThreeB = 3*(Mu*(VW.*WU) + V*(MuU.*WW) + W*(MuW.*VU));
-    ThreeC = 3*(Mv*(WW.*UU) + W*(MvU.*UW) + U*(MvW.*WU));
-    ThreeD = 3*(Mw*(UW.*VU) + U*(MwU.*VW) + V*(MwW.*UU));
+    ThreeA = 3*(Ms*(SU.*SW) + S*((MsU.*SW) + (MsW.*SU))); %Jv'*Js*vec(Ms)
+    ThreeB = 3*(Mu*(VW.*WU) + V*(MuU.*WW) + W*(MuW.*VU)); %Jv'*Ju*vec(Mu)
+    ThreeC = 3*(Mv*(WW.*UU) + W*(MvU.*UW) + U*(MvW.*WU)) + beta*Mv; %Jv'*Jv*vec(Mv)
+    ThreeD = 3*(Mw*(UW.*VU) + U*(MwU.*VW) + V*(MwW.*UU)); %Jv'*Jw*vec(Mw)
     %Jw Operations
-    FourA = 3*(Ms*(SU.*SV) + S*((MsU.*SV) + (MsV.*SU)));
-    FourB = 3*(Mu*(VU.*WV) + V*(MuV.*WU) + W*(MuU.*VV));
-    FourC = 3*(Mv*(WU.*UV) + W*(MvV.*UU) + U*(MvU.*WV));
-    FourD = 3*(Mw*(UU.*VV) + U*(MwV.*VU) + V*(MwU.*UV));
+    FourA = 3*(Ms*(SU.*SV) + S*((MsU.*SV) + (MsV.*SU))); %Jw'*Js*vec(Ms)
+    FourB = 3*(Mu*(VU.*WV) + V*(MuV.*WU) + W*(MuU.*VV)); %Jw'*Ju*vec(Mu) 
+    FourC = 3*(Mv*(WU.*UV) + W*(MvV.*UU) + U*(MvU.*WV)); %Jw'*Jv*vec(Mv)
+    FourD = 3*(Mw*(UU.*VV) + U*(MwV.*VU) + V*(MwU.*UV)) + beta*Mw; %Jw'*Jw*vec(Mw)
     
     % Join them together
     One = OneA + OneB + OneC + OneD + lambda*Ms;
@@ -83,5 +110,5 @@ function JJW = Apply_Approx_Hessian(Mat, M, lambda)
     Three = ThreeA + ThreeB + ThreeC + ThreeD + lambda*Mv;
     Four = FourA + FourB + FourC + FourD + lambda*Mw;
     % Return Vectorized result
-    JJW = [One(:); Two(:); Three(:); Four(:)];
+    JJx = [One(:); Two(:); Three(:); Four(:)];
 end
